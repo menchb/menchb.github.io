dcb67fbf594a389c470806d9805a577450f27396
Menchie <menchiebarcenas.belarmino@bicol-u.edu.ph>
2022-06-09 10:06:05 +0800

    Create overview and construction notice

---
+++ script.js
@@ -0,0 +1,10 @@
+var notice = `
+    <div class="constructionNotice">
+        <h1>Hi, this page is still under construction.</h1>
+        <p>You may come back later or connect with me through <a href="https://www.linkedin.com/in/menchie-belarmino/">LinkedIn</a>.</p>
+    </div>
+`
+
+function displayNotice() {
+    document.querySelector("main").innerHTML = notice;
+}
