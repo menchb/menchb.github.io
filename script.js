5ab85ac6e5920babbe6b03a1cea8918a6d2bccb1
Menchie <menchiebarcenas.belarmino@bicol-u.edu.ph>
2022-06-10 18:08:32 +0800

    Transfer page construction notice from js to html

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var notice = `
-    <div class="constructionNotice">
-        <h1>Hi, this page is still under construction.</h1>
-        <p>You may come back later or connect with me through <a href="https://www.linkedin.com/in/menchie-belarmino/">LinkedIn</a>.</p>
-    </div>
-`
+const constructionNotice = document.querySelector("#constructionNotice");
 
 function displayNotice() {
-    document.querySelector("main").innerHTML = notice;
+    tabs.forEach((tab) => {
+        tab.style.display = "none";
+    });
+    constructionNotice.style.display = "block";
 }
+
