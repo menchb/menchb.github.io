a2388aeb1b15644926f8f8177f577584dee8f16b
Menchie <menchiebarcenas.belarmino@bicol-u.edu.ph>
2022-06-10 18:15:52 +0800

    Add education tab

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const main = document.querySelector("main");
+const tabs = document.querySelectorAll("div");
+const overviewTab = document.querySelector("#overview");
+const educTab = document.querySelector("#education");
 const constructionNotice = document.querySelector("#constructionNotice");
 
 function displayNotice() {
@@ -5,5 +9,29 @@ function displayNotice() {
         tab.style.display = "none";
     });
     constructionNotice.style.display = "block";
+}
+
+function displayOverview() {
+    tabs.forEach((tab) => {
+        tab.style.display = "none";
+    });
+    overviewTab.style.display = "block";
+}
+
+function displayEducation(){
+    tabs.forEach((tab) => {
+        tab.style.display = "none";
+    });
+    educTab.style.display = "block";
+}
+
+const sections = document.querySelectorAll("section")
+
+function expandDetails(paramId) {
+    sections.forEach((detail) => {
+        detail.style.display = "none";
+    });
+    var sectionToExpand = document.querySelector(`#${paramId}`);
+    sectionToExpand.style.display = "block";
 }
 
